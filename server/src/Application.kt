ae846021b3505174fb9bb3c03f1472392158e9bf
David Herman <bit.spittle@gmail.com>
2020-01-13 21:30:58 -0800

    Initial commit

---
+++ server/src/Application.kt
@@ -0,0 +1,15 @@
+@file:JvmName("Application")
+
+package io.bitspittle.audioconductor
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+}
+
